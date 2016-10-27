42dcffbd288461c5a1599cd105374c0406dfc51d
Bawiesiecdromem <Bawiesiecdromem@gmail.com>
2016-10-27 09:39:43 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
-<HTML>
-<HEAD>
-
-<TITLE>Test</TITLE>
-</HEAD
-<BODY>
-<?php
-echo "hello world!";
-?>
-</BODY>
-</HTML>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Przepiórka</title>
+    </head>
+    <body>
+        <script>
+            var $folder_path="/v.";
+            var $version="0.1a";
+            var &path=$folder_path+$version+"/index.php";
+        </script>
+        <a href="/v.0.1a/index.php"></a>
+        <?php
+            
+        ?>
+        
+    </body>
+</html>
