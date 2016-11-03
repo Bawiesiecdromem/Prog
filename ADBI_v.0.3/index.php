66f62eb3251fbbf8ef2116af0cad909458b4e07a
Bawiesiecdromem <Bawiesiecdromem@gmail.com>
2016-11-03 09:35:44 +0100

    Add files via upload

---
+++ ADBI_v.0.3/index.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Rejstracja</title>
+    </head>
+    <body>
+        <form action="fregister" method="POST">
+            E-mail:<input type="text" name="fu_email"></input>
+            Nick:<input type="text" name="fu_nick"></input>
+            Hasło:<input type="text" name="fu_password"></input>
+            Imię:<input type="text" name="fu_name"></input>
+            Nazwisko:<input type="text" name="fu_forename"></input>
+            Telefon:<input type="text" name="fu_phone"></input>
+            Data urodzenia:<input type="text" name="fu_birth"></input>
+            <input type="submit" name="formsend" value="Wyślij!"></input>
+        </form>
+        <?php
+        
+        ?>
+    </body>
+</html>
