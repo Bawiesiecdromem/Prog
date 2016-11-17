9a8d0bddd7300e772a06b5d2c976ca36e043e02f
Bawiesiecdromem <Bawiesiecdromem@gmail.com>
2016-11-17 20:34:49 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
                                     <?php
                                         if ($_SESSION['userverificationkey']){
                                             echo '<li class="dropdown">
-                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">'.$_SESSION['u_nick'].'<span class="caret"></span></a>
+                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">'.$_SESSION['u_nick'].'<img src='.$_SESSION['u_avatar'].' class="adbiavatar" alt="{{Nick}}"><span class="caret"></span></a>
                                         <ul class="dropdown-menu">
                                             <li><a href="">{{MyAds}}</a></li>
                                             <li role="separator" class="divider"></li>
