44898bf14d221ce0ec49f8e2fdda507a78ceacdb
Bawiesiecdromem <Bawiesiecdromem@gmail.com>
2016-11-23 21:06:15 +0100

    T.T

---
+++ InsertCategories.sql
@@ -0,0 +1,27 @@
+use ADBI_DB;
+INSERT INTO T_CATEGORIES (cat_id, cat_name, cat_desc, mature_content)
+VALUES (1,'Post','Zwykły Post',0);
+INSERT INTO T_CATEGORIES (cat_id, cat_name, cat_desc, mature_content)
+VALUES (2,'PhotoPost','Foto-Post, jak na instagramie',0);
+INSERT INTO T_CATEGORIES (cat_id, cat_name, cat_desc, mature_content)
+VALUES (3,'Motoryzacja','Ogłoszenie',0);
+INSERT INTO T_CATEGORIES (cat_id, cat_name, cat_desc, mature_content)
+VALUES (4,'Elektronikia','Ogłoszenie',0);
+INSERT INTO T_CATEGORIES (cat_id, cat_name, cat_desc, mature_content)
+VALUES (5,'Nieeruchomości','Ogłoszenie',0);
+INSERT INTO T_CATEGORIES (cat_id, cat_name, cat_desc, mature_content)
+VALUES (6,'Moda','Ogłoszenie',0);
+INSERT INTO T_CATEGORIES (cat_id, cat_name, cat_desc, mature_content)
+VALUES (7,'Sporthobby','Ogłoszenie',0);
+INSERT INTO T_CATEGORIES (cat_id, cat_name, cat_desc, mature_content)
+VALUES (8,'Praca','Ogłoszenie',0);
+INSERT INTO T_CATEGORIES (cat_id, cat_name, cat_desc, mature_content)
+VALUES (9,'Rolnictwo','Ogłoszenie',0);
+INSERT INTO T_CATEGORIES (cat_id, cat_name, cat_desc, mature_content)
+VALUES (10,'Edukacja','Ogłoszenie',0);
+INSERT INTO T_CATEGORIES (cat_id, cat_name, cat_desc, mature_content)
+VALUES (11,'Muzyka','Ogłoszenie',0);
+INSERT INTO T_CATEGORIES (cat_id, cat_name, cat_desc, mature_content)
+VALUES (12,'Dom i ogród','Ogłoszenie',0);
+INSERT INTO T_CATEGORIES (cat_id, cat_name, cat_desc, mature_content)
+VALUES (13,'Zwierzęta','Ogłoszenie',0);
