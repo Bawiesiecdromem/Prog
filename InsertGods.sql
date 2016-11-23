16fff25fcf23142b9e0c74d1d18f0321575010a0
Bawiesiecdromem <Bawiesiecdromem@gmail.com>
2016-11-23 20:02:09 +0100

    *.*

---
+++ InsertGods.sql
@@ -0,0 +1,9 @@
+use ADBI_DB;
+INSERT INTO T_USERS (u_id, u_email, u_nick, u_password, u_name, u_forename, u_phone, u_birth, u_date, u_avatar, u_god)
+VALUES (1,'NoFaceGod@nf.god','NoFaceGod','NFG','Kaonashi','No-Face',666666666,'1111-11-11','1111-11-11','pics/u_avatar/1_NoFaceGod.jpg',1);
+INSERT INTO T_USERS (u_id, u_email, u_nick, u_password, u_name, u_forename, u_phone, u_birth, u_date, u_avatar, u_god)
+VALUES (2,'JohnSnow@js.js','JohnSnow998th','John','John','Snow',123123123,'1997-11-11','2016-09-01','pics/u_avatar/2_JohnSnow.png',2);
+INSERT INTO T_USERS (u_id, u_email, u_nick, u_password, u_name, u_forename, u_phone, u_birth, u_date, u_avatar, u_god)
+VALUES (3,'RamsayBolton@rb.rb','RamsayBoltonNotSnow','Ramsay','Ramsay','Bolton',123123123,'1997-12-12','2016-09-02','pics/u_avatar/3_RamsayBolton.jpg',2);
+INSERT INTO T_USERS (u_id, u_email, u_nick, u_password, u_name, u_forename, u_phone, u_birth, u_date, u_avatar)
+VALUES (4,'JS@js.js','JohnSnowTheFirstOfHisName','John','John','Snow',123454321,'1997-01-01','2016-10-10','pics/u_avatar/4.jpg');
