f8518c0b074eca6772cee49a4176fad173f52c20
Jimmysvv <bizdebski@yahoo.com>
2016-11-27 22:52:37 +0100

    Add files via upload

--- f_register.php
+++ f_register.php
@@ -25,7 +25,7 @@ if($submit){
                 exit;
             }
             else{
-            $query = mysqli_query($con,"INSERT INTO T_USERS (u_email, u_nick, u_password, u_name, u_forename, u_phone, u_birth, u_date) VALUES ('$u_email','$u_nick','$u_password','$u_name','$u_forename','$u_phone','$u_birth','$u_date')");
+            $query = mysqli_query($con,"INSERT INTO T_USERS (u_email, u_nick, u_password, u_name, u_forename, u_phone, u_birth, u_date, u_avatar) VALUES ('$u_email','$u_nick','$u_password','$u_name','$u_forename','$u_phone','$u_birth','$u_date','pics/u_avatar/Undefined.jpg')");
             echo 'Pomyślnie zarejestrowano';
             $session_query = mysqli_query($con,"SELECT * FROM T_USERS WHERE u_email='$u_email'");
             $numrows = mysqli_num_rows($session_query);
