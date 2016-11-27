330b43b4ff7c644d77134ca66d828de55a31b537
Jimmysvv <bizdebski@yahoo.com>
2016-11-27 21:56:32 +0100

    Add files via upload

--- f_changephoto.php
+++ f_changephoto.php
@@ -1,18 +1,26 @@
 <?php
 include 'f_sessionuserdatacreate.php';
 $u_id = $_SESSION['u_id'];
+$image_info = getimagesize($_FILES['favatar']['tmp_name']);
+$image_width = $image_info[0];
+$image_height = $image_info[1];
+
 $path = 'pics/u_avatar/';
 $u_avatar = $path.basename($_FILES['favatar']['name']);
 
 if(isset($_POST['fchange'])){
-
+if($image_width==$image_height){
 if(move_uploaded_file($_FILES['favatar']['tmp_name'], $u_avatar)){ 
 $con = mysqli_connect('localhost','root','','ADBI_DB') or die ('Nie można nawiązać połączenia');
 $query = mysqli_query($con, "UPDATE T_USERS SET u_avatar='$u_avatar' WHERE u_id='$u_id'");
-echo 'jp';
+echo 'dziala';
 } 
-else{
-echo 'eror';
+	else{
+	echo 'format 1:1';
+	}
 }
+	else{
+	echo 'eror';
+	}
 }
 ?>
