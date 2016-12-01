62e9550b76cb7b428775428a45074eb620fba760
Jimmysvv <bizdebski@yahoo.com>
2016-12-01 22:54:52 +0100

    Dolary (╭☞$ヮ$)╭☞
    Ludzie dla pieniędzy są w stanie zrobić wszystko, nawet pójść do pracy

--- f_frozead.php
+++ f_frozead.php
@@ -1,7 +1,7 @@
 <?php
 $con = mysqli_connect('localhost','root','','ADBI_DB') or die ('Nie można nawiązać połączenia');
-$archive = mysqli_query($con,"INSERT INTO T_FROZENAD SELECT * FROM T_AD WHERE `ad_date` < DATE_SUB(NOW(), INTERVAL 20 MINUTE)");
+$archive = mysqli_query($con,"INSERT INTO T_FROZENAD SELECT * FROM T_AD WHERE `ad_transferdate` = NOW()");
 $x = mysqli_query($con,"SET foreign_key_checks = 0");
-$deletepost = mysqli_query($con,"DELETE FROM T_AD WHERE `ad_date` < DATE_SUB(NOW(), INTERVAL 20 MINUTE)");
+$deletepost = mysqli_query($con,"DELETE FROM T_AD WHERE `ad_transferdate` = NOW()");
 $d =  mysqli_query($con, "foreign_key_checks = 1");
 ?>
