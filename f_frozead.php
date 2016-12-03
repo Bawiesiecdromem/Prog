d70bf65d1806f94dac9c51ef72551b1519727c26
Bawiesiecdromem <Bawiesiecdromem@gmail.com>
2016-12-03 20:48:26 +0100

    *.*
    T.T

--- f_frozead.php
+++ f_frozead.php
@@ -1,7 +1,7 @@
 <?php
-include 'config/serverconfig.php';
-$archive = mysqli_query($con,"INSERT INTO T_FROZENAD SELECT * FROM T_AD WHERE ad_transferdate = NOW()");
-$x = mysqli_query($con,"SET foreign_key_checks = 0");
-$deletepost = mysqli_query($con,"DELETE FROM T_AD WHERE ad_transferdate = NOW()");
-$d =  mysqli_query($con, "foreign_key_checks = 1");
+    include 'config/serverconfig.php';
+    $archive = mysqli_query($con,"INSERT INTO T_FROZENAD SELECT * FROM T_AD WHERE `ad_transferdate` <= NOW()");
+    $x = mysqli_query($con,"SET foreign_key_checks = 0");
+    $deletepost = mysqli_query($con,"DELETE FROM T_AD WHERE `ad_transferdate` <= NOW()");
+    $d =  mysqli_query($con, "foreign_key_checks = 1");
 ?>
